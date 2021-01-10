a3289b11e40ba80f674d6c347a0b47d3c858aa20
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-01-10 13:06:30 -0500

    simple module exports

--- Week1/NodeJS/node-examples/index.js
+++ Week1/NodeJS/node-examples/index.js
@@ -3,8 +3,19 @@
 //     area: (x,y) => (x*y)
 // }
 
-let rect = require('./rectangle')
+// function solveRect(l,b){
+//     console.log(`Solving for rectangle with l = ${l} and b = ${b}`)
+//     if(l <=0 || b <=0){
+//         console.log(`Rectangle dimensions should be greater than zero: l= ${l} and b = ${b}`)
+//     }else{
+//         console.log(`The area of the rectangle is ${rect.area(l,b)}`)
+//         console.log(`The perimeter of the rectangle is ${rect.perimeter(l,b)}`)
+//     }
+// }
 
+
+//Using the module require
+let rect = require('./rectangle')
 
 function solveRect(l,b){
     console.log(`Solving for rectangle with l = ${l} and b = ${b}`)
--- Week1/NodeJS/node-examples/rectangle.js
+++ Week1/NodeJS/node-examples/rectangle.js
@@ -1,2 +1,13 @@
-exports.perimeter = (x,y) => (2*(x+y))
-exports.area = (x,y) => (x*y)
+// exports.perimeter = (x,y) => (2*(x+y))
+// exports.area = (x,y) => (x*y)
+
+
+// Another way of export
+// When export multiple function, module.exports is required cannot use short hand exports
+let perimeter = (x,y) => (2*(x+y))
+let area = (x,y) => (x*y)
+
+module.exports = {
+    perimeter,
+    area
+}
