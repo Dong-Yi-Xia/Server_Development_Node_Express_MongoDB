571643b81299c731d2da91f2fff8931d1346aea7
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-01-13 12:18:08 -0500

    server static page express morgan

--- Week1/04-node_express/index.js
+++ Week1/04-node_express/index.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const http = require('http')
+const morgan = require('morgan')
 
 const hostname = 'localhost'
 const port = 3000 
 
 const app = express()
+app.use(morgan('dev'))
+
+//__dirname => rootfolder
+app.use(express.static(__dirname+ '/public'))
 
 //buildin function .use() take 3 parameter. 
 // Next is optional, Next allow to use more middleware
 app.use((req, res, next) => {
-    console.log(req.headers)
     res.statusCode = 200
     res.setHeader('Content_Type', 'text/html')
     res.end('<html><body><h1>This is an Express Server</h1></body></html>')
@@ -20,3 +24,5 @@ const server = http.createServer(app)
 server.listen(port, hostname, ()=> {
     console.log(`Server running at http://${hostname}:${port}`)
 })
+
+
