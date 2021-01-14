ec63a2d98401779c73897e830aeb5f1b69e0a11d
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-01-14 18:01:13 -0500

    express router

---
+++ Week1/06-node_express_Router/index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const http = require('http')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+
+//dishRouter is a filepath  
+const dishRouter = require('./routes/dishRouter')
+
+const hostname = 'localhost'
+const port = 3000 
+
+const app = express()
+//Everytime you use a middleware include app.use
+app.use(morgan('dev'))
+app.use(bodyParser.json()) //in the req, the body is parsed into JSON
+
+//mounting the dishRouter, Forwards any requests to the /dishes URI to our dishRouter
+//1st parameter is the endpoint, 2nd parameter is the express Router
+app.use('/dishes', dishRouter)
+
+
+
+//__dirname => rootfolder
+app.use(express.static(__dirname+ '/public'))
+
+//buildin function .use() take 3 parameter. 
+// Next is optional, Next allow to use more middleware
+app.use((req, res, next) => {
+    res.statusCode = 200
+    res.setHeader('Content_Type', 'text/html')
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>')
+})
+
+const server = http.createServer(app)
+
+server.listen(port, hostname, ()=> {
+    console.log(`Server running at http://${hostname}:${port}`)
+})
+
+
