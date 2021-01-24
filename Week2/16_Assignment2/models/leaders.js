8b4b54face1fc98802078262221a8b4187e6016c
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-01-24 12:31:16 -0500

    assignment 02

---
+++ Week2/16_Assignment2/models/leaders.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const Scheme = mongoose.Schema
+
+
+const leaderSchema = new Scheme({
+    name:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    image:{
+        type: String,
+        required: true
+    },
+    designation:{
+        type: String,
+        required: true
+    },
+    abbr:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    featured:{
+        type: Boolean,
+        required: true
+    }
+})
+
+let Leader = mongoose.model('Leader', leaderSchema)
+
+module.exports = Leader
