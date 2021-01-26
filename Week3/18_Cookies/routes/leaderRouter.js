7b819df92dbe0e4fb6672e3d2bd38d43e07d4617
Dong-Yi-Xia <dong7777xia@gmail.com>
2021-01-26 15:12:42 -0500

    cookies

---
+++ Week3/18_Cookies/routes/leaderRouter.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const Leader = require('../models/leaders')
+
+const leadersRouter = express.Router()
+leadersRouter.use(bodyParser.json())
+
+leadersRouter.route('/')
+.get((req,res,next) => {
+    Leader.find({})
+    .then(leaders => {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json')
+        res.json(leaders)
+    }, err => next(err))
+    .catch(err => next(err))
+})
+
+.post((req,res,next) => {
+    Leader.create(req.body)
+    .then(leader => {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json')
+        res.json(leader)
+    }, err => next(err))
+    .catch(err => next(err))
+})
+
+.put((req,res,next) => {
+    res.statusCode = 403
+    res.end(`PUT operation not supported on /leaders`)
+})
+
+.delete((req,res,next) => {
+    Leader.remove({})
+    .then(result => {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json')
+        res.json(result)
+    }, err => next(err))
+    .catch(err => next(err))
+})
+
+
+////////////////////////////////////////////
+////////////////////////////////////////////
+
+leadersRouter.route('/:leaderId')
+.get((req,res,next) => {
+    Leader.findById(req.params.leaderId)
+    .then(leader => {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json')
+        res.json(leader)
+    }, err => next(err))
+    .catch(err => next(err))
+})
+
+.post((req,res,next) => {
+    res.statusCode = 403
+    res.end(`POST operation not supported on /leaders ${req.params.leaderId}`)
+})
+
+.put((req,res,next) => {
+    Leader.findByIdAndUpdate(req.params.leaderId, 
+        {$set: req.body}, 
+        {new: true}
+    )
+    .then(leader => {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json')
+        res.json(leader)
+    }, err => next(err))
+    .catch(err => next(err))
+})
+
+.delete((req,res,next) => {
+    Leader.findByIdAndRemove(req.params.leaderId)
+    .then(result => {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json')
+        res.json(result)
+    }, err => next(err))
+    .catch(err => next(err))
+})
+
+module.exports = leadersRouter
